22e77af30ef3a3937ed91c9135027540991ba017
f <franklinrohan2002@gmail.com>
2023-11-29 17:59:44 +0530

    create put,get,delect endpoints

--- api/server.js
+++ api/server.js
@@ -20,4 +20,31 @@ app.get("/todos", async (req, res) => {
   res.json(todos);
 });
 
+app.post("/todo/new", (req, res) => {
+  const todo = new Todo({
+    text: req.body.text,
+  });
+  todo.save();
+  res.json(todo);
+});
+
+app.delete("/todo/delete/:id", async (req, res) => {
+  try {
+    const result = await Todo.findByIdAndDelete(req.params.id);
+    if (!result) {
+      return res.status(404).json({ error: "Todo not found" });
+    }
+    return res.json(result);
+  } catch (err) {
+    return res.status(500).json({ error: "Internal server error" });
+  }
+});
+app.put("/todo/update/:id", async (req, res) => {
+  const todo = await Todo.findById(req.params.id);
+
+  todo.complete = !todo.complete;
+  todo.save();
+  res.json(todo);
+});
+
 app.listen(3002, () => console.log("Server stateted on port 3001"));
