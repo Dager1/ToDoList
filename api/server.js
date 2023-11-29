4480420918c715a57d1dc6cd3376d44eb6de882f
f <franklinrohan2002@gmail.com>
2023-11-29 14:18:34 +0530

    get request of all the todos

--- api/server.js
+++ api/server.js
@@ -12,4 +12,12 @@ mongoose
   .then(() => console.log("connected to DB"))
   .catch(console.error);
 
+const Todo = require("./models/Todo");
+
+app.get("/todos", async (req, res) => {
+  const todos = await Todo.find();
+
+  res.json(todos);
+});
+
 app.listen(3002, () => console.log("Server stateted on port 3001"));
